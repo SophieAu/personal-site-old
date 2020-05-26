db716aceb6c5ddd17eb3a33cb714a84a63c3e817
Sophie Au <SophieAu@users.noreply.github.com>
2020-05-26 18:30:25 +0200

    Make SEO component nicer

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -18,46 +18,16 @@ const SEO: React.FC<Props> = ({ title, description, slug, children }) => (
     }}
     title={title}
     meta={[
-      {
-        name: `description`,
-        content: description,
-      },
-      {
-        property: `og:title`,
-        content: title,
-      },
-      {
-        property: `og:description`,
-        content: description,
-      },
-      {
-        property: `og:type`,
-        content: `website`,
-      },
-      {
-        property: `og:url`,
-        content: `${BASE_URL}/${slug}`,
-      },
-      {
-        name: `twitter:card`,
-        content: `summary`,
-      },
-      {
-        name: `twitter:url`,
-        content: `${BASE_URL}/${slug}`,
-      },
-      {
-        name: `twitter:title`,
-        content: title,
-      },
-      {
-        name: `twitter:description`,
-        content: description,
-      },
-      {
-        name: `twitter:site`,
-        content: TWITTER_URL,
-      },
+      { name: `description`, content: description },
+      { property: `og:title`, content: title },
+      { property: `og:description`, content: description },
+      { property: `og:type`, content: `website` },
+      { property: `og:url`, content: `${BASE_URL}/${slug}` },
+      { name: `twitter:card`, content: `summary` },
+      { name: `twitter:url`, content: `${BASE_URL}/${slug}` },
+      { name: `twitter:title`, content: title },
+      { name: `twitter:description`, content: description },
+      { name: `twitter:site`, content: TWITTER_URL },
     ]}
   >
     {children}
